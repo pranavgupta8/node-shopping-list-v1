856b600c58c2637940dd6796d3dae385330da689
pranavgupta8 <pranavgupta8@hotmail.com>
2017-03-11 20:09:28 +0530

    GET routing for /recipes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
 
+//
+const{Recipes} = require('./models');
+
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -18,10 +21,20 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+//adding some items to the recipes list
+Recipes.create('Chocolate Milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('Cheese Sandwich', ['bread', 'cheese', 'italian seasoning']);
+Recipes.create('Traditional Spagetti', ['Spagetti', 'tomatoes', 'salt & pepper', 'seasoning', 'olive oil']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+//routing for GET request for recipes
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
